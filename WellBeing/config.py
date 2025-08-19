9cbb27ce097563527c73af17ac76efe841a5de8f
MLee2205 <jecy.meffo@facsciences-uy1.cm>
2025-08-19 22:44:19 +0100

    ajout de l url de railway

--- WellBeing/config.py
+++ WellBeing/config.py
@@ -1,7 +1,11 @@
+import os
+
 class Config:
-    SECRET_KEY = 'votre_cle_secrete'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'votre_cle_secrete'
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mlee:mlee@localhost/mealplanner'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://mealplanner_84jp_user:mXPm4fNnq1bHoUgL7AoMULE8ypg56SjM@dpg-d2i5n4je5dus73eenee0-a/mealplanner_84jp'
-
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://mealplanner_84jp_user:mXPm4fNnq1bHoUgL7AoMULE8ypg56SjM@dpg-d2i5n4je5dus73eenee0-a/mealplanner_84jp'
+    
+    # URL Railway - remplacez par votre URL Railway
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:qMfLPDVhpkXTEdNsaMsuKibUMjeZpAnR@nozomi.proxy.rlwy.net:45585/railway'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
