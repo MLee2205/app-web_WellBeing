f22e9b79e3c03966710bc2966347801cb1d33d63
MLee2205 <jecy.meffo@facsciences-uy1.cm>
2025-08-19 14:04:51 +0100

    modif de l url dans config

--- WellBeing/config.py
+++ WellBeing/config.py
@@ -1,5 +1,7 @@
 class Config:
     SECRET_KEY = 'votre_cle_secrete'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mlee:mlee@localhost/mealplanner'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mlee:mlee@localhost/mealplanner'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://mealplanner_84jp_user:mXPm4fNnq1bHoUgL7AoMULE8ypg56SjM@dpg-d2i5n4je5dus73eenee0-a/mealplanner_84jp'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
