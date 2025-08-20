7e97684db44a3a648a79ab91d4f7a27b1aed1b48
MLee2205 <jecy.meffo@facsciences-uy1.cm>
2025-08-20 07:29:09 +0100

    modif du nom de la bd,ajout du s

--- WellBeing/models/user.py
+++ WellBeing/models/user.py
@@ -5,6 +5,7 @@ from datetime import date
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
